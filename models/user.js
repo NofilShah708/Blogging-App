cac06e353af7a20aab410b7330a31ccae53ae256
NofilShah <150585863+NofilShah708@users.noreply.github.com>
2025-02-22 18:01:35 +0500

    Initial commit

--- models/user.js
+++ models/user.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb://localhost:27017/minisocialmediaapp`);
-
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI);
 const userSchema = mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-    posts: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'post',
-    }],
-    profilepic: {
-        type: String,
-        default: 'default.webp'
-    }
-})
+  username: String,
+  email: String,
+  password: String,
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
+  profilepic: {
+    type: String,
+    default: "default.webp",
+  },
+});
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
