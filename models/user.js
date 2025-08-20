2068ac8cdf8d75a8be9bd45e11d8d55f76d71c11
NofilShah <150585863+NofilShah708@users.noreply.github.com>
2025-08-20 15:11:18 +0500

    `Removed like feature implementation and Vercel configuration`

--- models/user.js
+++ models/user.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDataBase");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const userSchema = mongoose.Schema({
   username: String,
   email: String,
